ae20fd3667aa6cf6878aa4eb71975433fce1a662
Callum Wellwood-Kane <business@callumwk.com>
2024-04-21 22:16:54 +1000

    updated worker and code

---
+++ schema.sql
@@ -0,0 +1,3 @@
+drop table if exists Links;
+create table if not exists Links (id integer primary key, url text, short text);
+insert into Links
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Sqids from 'sqids';
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -11,8 +12,54 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface Env {
+	short_urls: KVNamespace;
+	SECRET_KEY: string;
+	DB: D1Database;
+}
+
+const squids = new Sqids({ alphabet: 'abcdefghijklmnopqrstuvwxyz0123456789' });
+
+async function createDbEntry(env: Env, url: string) {
+	const res = await env.DB.prepare('insert into Links (url) values (?)').bind(url).run();
+	return res.meta.last_row_id;
+}
+
+async function getOriginalUrl(env: Env, pathname: string) {
+	const shortUrl = pathname.slice(1);
+	const [id] = squids.decode(shortUrl);
+	const res = await env.DB.prepare('select url from Links where id = ?').bind(id).first<{ url: string }>();
+	return res?.url;
+}
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		// This is the url that is requested
+		const incomingUrl = new URL(request.url);
+		const pathname = incomingUrl.pathname;
+		const params = incomingUrl.searchParams;
+
+		if (pathname === '/') return new Response('url shortner');
+
+		if (pathname === '/favicon.ico') return new Response(null, { status: 404 });
+
+		const createKey = params.get('key');
+		if (pathname === '/create') {
+			if (createKey !== env.SECRET_KEY) return new Response('No key provided');
+			const url = params.get('url');
+			if (url) {
+				const id = await createDbEntry(env, url);
+				const shortUrl = squids.encode([id]);
+				return new Response(`Short url created /${shortUrl}`);
+			}
+		}
+
+		const redirectUrl = await getOriginalUrl(env, pathname);
+
+		if (redirectUrl) {
+			return Response.redirect(redirectUrl, 302);
+		}
+
+		return new Response(`<html><p>There is no url defined for the following ${pathname}</p></html>`);
 	},
 };
